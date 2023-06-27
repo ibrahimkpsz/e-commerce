b7e7bc71d9b260d951a1e7ea9acc4e957fe4578d
ibrahimkpsz <ibrahimkpsz@outlook.com>
2023-06-27 17:51:00 +0300

    Completed 40%

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -2,11 +2,16 @@ import React, { useEffect, useState } from 'react'
 import '../styles/products.css'
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCartPlus, faStar } from '@fortawesome/free-solid-svg-icons';
+import { faCartPlus, faMagnifyingGlass, faStar } from '@fortawesome/free-solid-svg-icons';
 
 function Products() {
 
+  // List Product 
+
   const [data, setData] = useState([]);
+  const [minPrice, setMinPrice] = useState('');
+  const [maxPrice, setMaxPrice] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
   useEffect(() => {
     const products = async () => {
@@ -18,6 +23,8 @@ function Products() {
     
   }, []);
 
+  // Sort Product Price
+
   const sortProductsByPrice = (minPrice, maxPrice) => {
     const sortedProducts = data.filter(
       (product) => product.price >= minPrice && product.price <= maxPrice
@@ -28,45 +35,108 @@ function Products() {
     return sortedProducts;
   };
   
-  const [minPrice, setMinPrice] = useState('');
-  const [maxPrice, setMaxPrice] = useState('');
+  
 
   const sortedProducts = sortProductsByPrice(minPrice, maxPrice);
 
+
+  // Sort Category
+  const categories = data.map(product => product.category);
+  const uniqueCategories = [... new Set(categories)]
+
+  const filteredCategory = data.filter(product => (product.category === selectedCategory));
+  const product_title = document.querySelectorAll(".card-title");
+  
+
+  
+  // Search
+  const handleSearch = (e) => {
+    const searchValue = e.target.value.toLowerCase();
+    product_title.forEach((title) => {
+      const parentElement = title.parentElement.parentElement.parentElement;
+      parentElement.classList.remove("d-block");
+      parentElement.classList.remove("d-none");
+
+      if(title.innerHTML.toLowerCase().includes(searchValue)) {
+        parentElement.classList.add("d-block")
+      } else {
+        parentElement.classList.add("d-none")
+      }
+    })
+  }
+  
+  
   
   return (
-    <div className='container'>
+    <div className='container pb-5'>
       <div className='d-flex justify-content-between align-items-center'>
         <h2 className='products-title my-4'>Products</h2>
-        <button class="btn buy-btn h-50" type="button" data-bs-toggle="offcanvas" data-bs-target="#filter" aria-controls="filter">Filter</button>
+        <button className="btn buy-btn h-50" type="button" data-bs-toggle="offcanvas" data-bs-target="#filter" aria-controls="filter">Filter</button>
       </div>
 
-      <div class="offcanvas offcanvas-end" tabindex="-1" id="filter" aria-labelledby="filterLabel">
-        <div class="offcanvas-header">
-          <h5 class="offcanvas-title" id="filterLabel">Filter</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      <div className="offcanvas offcanvas-end" tabindex="-1" id="filter" aria-labelledby="filterLabel">
+        <div className="offcanvas-header">
+          <h5 className="offcanvas-title" id="filterLabel">Filter</h5>
+          <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
-        <div class="offcanvas-body">
-          {/* Price Filter */}
-          <p>Price :</p>
+        <div className="offcanvas-body">
+
+          {/* Search */}
+
           <form>
-              <div className="row">
-                <div className="col">
-                  <input type="text" id='minPrice' value={minPrice} onChange={(e) => setMinPrice(Number(e.target.value))} className='form-control' placeholder='Min' />
-                </div>
-                <div className="col">
-                  <input type="text" id='maxPrice' value={maxPrice} onChange={(e) => setMaxPrice(Number(e.target.value))} className='form-control' placeholder='Max' />
-                </div>
-              </div>
+            <label htmlFor="searchInput">Search : </label>
+            <div className="input-group align-items-center">
+              <input type="text" id='searchInput' onChange={handleSearch} className='form-control my-2' placeholder='Enter product name' />
+              <button className='btn buy-btn h-50' id='searchBtn' onClick={(e) => e.preventDefault()}>
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+              </button>
+            </div>
           </form>
+
+          {/* Category Filter */}
+
+          <div className='filterCategory mt-4'>
+            <p>Category :</p>
+            <select value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)} className="form-select">
+              <option value="">All</option>
+              {
+                uniqueCategories.map((product, index) => (
+                  <option key={index} value={product}>
+                    {product}
+                  </option>
+                ))
+              }
+            </select>
+          </div>
+
+          {/* Price Filter */}
+
+          <div className='sortPrice mt-4'>
+            <p>Price :</p>
+            <form>
+                <div className="row">
+                  <div className="col">
+                    <input type="text" id='minPrice' value={minPrice} onChange={(e) => setMinPrice(Number(e.target.value))} className='form-control' placeholder='Min' />
+                  </div>
+                  <div className="col">
+                    <input type="text" id='maxPrice' value={maxPrice} onChange={(e) => setMaxPrice(Number(e.target.value))} className='form-control' placeholder='Max' />
+                  </div>
+                </div>
+            </form>
+          </div>
+          
         </div>
       </div>
       
       <div className="row row-cols-1 row-cols-lg-4 row-cols-md-2 g-4">
       {
-        sortedProducts.length > 0 ? (
-       
-        sortedProducts.map((product) => (
+        (
+          sortedProducts.length > 0
+          ? sortedProducts
+          : filteredCategory.length > 0
+          ? filteredCategory
+          : data
+        ).map((product) => (
           <div className="col">
                 <div className="card rounded-0 h-100" key={product.id}>
                     <img src={product.image} alt={product.title} className='card-img-top img-fluid p-3'/>
@@ -83,28 +153,8 @@ function Products() {
                     </div>
                 </div>
           </div>
-          )
-        )) : (
-          data.map(product => 
-            <div className="col">
-                <div className="card rounded-0 h-100" key={product.id}>
-                    <img src={product.image} alt={product.title} className='card-img-top img-fluid p-3'/>
-                    <hr />
-                    <div className="card-body">
-                        <h5 className='card-title text-truncate'>{product.title}</h5>
-                        <p className='card-text text-truncate'>
-                            {product.rating.rate} <FontAwesomeIcon icon={faStar} style={{color: "#dbd539",}} />
-                        </p>
-                    </div>
-                    <div className="card-footer d-flex align-items-center justify-content-between">
-                        <div><b>Price:</b> {product.price}&#36;</div>
-                        <a href="#" className='btn buy-btn'><FontAwesomeIcon icon={faCartPlus} /></a>
-                    </div>
-                </div>
-            </div>
-            )
         )
-      
+        )  
       }
         
       </div>
